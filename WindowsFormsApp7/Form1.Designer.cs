db2b175ee16ff5fa542e6121b6bac28283ba494b
Bahar <irembahar4118@gmail.com>
2023-01-17 01:52:08 +0300

    TextYazdirma
    Girilen metni form üzerinde tıklanan bir konuma yazdırma

---
+++ WindowsFormsApp7/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace WindowsFormsApp7
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(129, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Yazdırmak istediğiniz ifadeyi giriniz : ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(310, 83);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(179, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 461);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ WindowsFormsApp7/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp7
+{
+    public partial class Form1 : Form
+    {
+        int click = 0;
+        Random rnd = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (click == 0)
+            {
+                fontDialog1.ShowColor = true;
+
+                fontDialog1.Font = textBox1.Font;
+                fontDialog1.Color = textBox1.ForeColor;
+
+                if (fontDialog1.ShowDialog() != DialogResult.Cancel)
+                {
+                    textBox1.Font = fontDialog1.Font;
+                    textBox1.ForeColor = fontDialog1.Color;
+                    click++;
+                }
+            }
+            else
+            {
+                switch (e.Button)
+                {
+
+                    case MouseButtons.Left:
+
+                        Label label = new Label();
+
+                        label.Location = new Point((e.X), e.Y);
+
+                        label.Text = textBox1.Text;
+                        label.Font = textBox1.Font;
+                        label.Height = textBox1.Height;
+                        label.Width = textBox1.Width;
+                        label.ForeColor = textBox1.ForeColor;
+                        this.Controls.Add(label);
+                        break;
+
+                    case MouseButtons.Middle:
+                        textBox1.Text = "";
+
+                        fontDialog1.ShowColor = true;
+
+                        fontDialog1.Font = textBox1.Font;
+                        fontDialog1.Color = textBox1.ForeColor;
+
+                        if (fontDialog1.ShowDialog() != DialogResult.Cancel)
+                        {
+                            textBox1.Font = fontDialog1.Font;
+                            textBox1.ForeColor = fontDialog1.Color;
+                        }
+                        break;
+                    case MouseButtons.Right:
+                        Label label2 = new Label();
+
+                        label2.Location = new Point(rnd.Next(0, 700), rnd.Next(0, 500));
+
+                        label2.Text = textBox1.Text;
+                        label2.Font = textBox1.Font;
+                        label2.Height = textBox1.Height;
+                        label2.Width = textBox1.Width;
+                        label2.ForeColor = textBox1.ForeColor;
+                        this.Controls.Add(label2);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
